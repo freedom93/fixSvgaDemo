c93549d95ebacc96be1c9b70df5d7416ff97cc1d
lifen <lifen@yy.com>
2018-05-15 18:39:29 +0800

    add dependences

--- svga-demo/src/App.vue
+++ svga-demo/src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div id="demoCanvas"></div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+// import SVGA from 'svgaplayerweb';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted() {
+    // const player = new SVGA.Player('#demoCanvas');
+    // const parser = new SVGA.Parser('#demoCanvas'); // Must Provide same selector eg:#demoCanvas IF support IE6+
+    // const svgUrl = `${location.origin}/assets/media/rose.svga`;
+    // parser.load(svgUrl, (videoItem) => {
+    //     player.setVideoItem(videoItem);
+    //     // player.setImage('//web2.yystatic.com/public/global/base/image/yybear2.jpg', '99');
+    //     player.setText({
+    //       text: 'Hello, Freedom!',
+    //       size: '20px',
+    //       color: '#000000',
+    //       offset: {x: 0.0, y: -10.0}
+    //     }, '_01hengfu');
+    //     player.startAnimation();
+    // });
   }
 }
 </script>
@@ -24,5 +40,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#demoCanvas{
+  width: 90%;
+  height: 90%;
+  background: rgba(255, 255, 255, 0.3);
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 </style>
